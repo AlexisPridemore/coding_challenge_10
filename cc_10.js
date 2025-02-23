c0f125ccb35e17a2a3e7eafd93b9bb05fd391b9c
Alexis Pridemore <alpridemore@usf.edu>
2025-02-23 16:13:46 -0500

    Added comments to Task 1

--- cc_10.js
+++ cc_10.js
@@ -2,24 +2,24 @@
 
 class Product {
     constructor(name, id, price, stock) {
-      this.name = name;
-      this.id =  id;
-      this.price = price;
-      this.stock = stock;
+      this.name = name;  //Product name
+      this.id =  id;     //Product ID
+      this.price = price;   //Product Price
+      this.stock = stock;   //Product Stock
     }
 getDetails() {
     return `Product: ${this.name}, ID: ${this.id}, Price: $${this.price}, Stock: ${this.stock}`;
-};
+};   //Display Product Details
 
-updateStock(quantity){
+updateStock(quantity){     //Decrease stock amount based on quantity ordered
     this.stock -= quantity;
-};
-restock(quantity) {
+};   
+restock(quantity) {        //Increase Sotkc by restock Quantity
     this.stock += quantity;
 }
 };
 
-const prod1 = new Product("Laptop", 101, 1200, 10); 
+const prod1 = new Product("Laptop", 101, 1200, 10);  //Declare new product
 console.log(prod1.getDetails());
-prod1.updateStock(3);
+prod1.updateStock(3);             //Decrease amount of stock by 3
 console.log(prod1.getDetails()); 

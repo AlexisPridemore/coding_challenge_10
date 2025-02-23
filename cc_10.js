28c98210c8e30fafe040753484a808345911aa12
Alexis Pridemore <alpridemore@usf.edu>
2025-02-23 16:35:22 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -23,3 +23,22 @@ const prod1 = new Product("Laptop", 101, 1200, 10);  //Declare new product
 console.log(prod1.getDetails());
 prod1.updateStock(3);             //Decrease amount of stock by 3
 console.log(prod1.getDetails()); 
+
+// Task 2: Creating an Order Class
+
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = product.price * quantity;
+        this.product.updateStock(quantity);
+    }
+    getOrderDetails() {
+        return `Order ID: $${this.orderId}, Product: $${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.totalPrice}`
+    };
+}
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+console.log(prod1.getDetails()); 
